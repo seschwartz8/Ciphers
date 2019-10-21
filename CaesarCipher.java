ee7bd63ef155f3c014725d957e3b7d8274209d13
Sasa Schwartz <seschwartz8@gmail.com>
2019-10-21 14:18:27 -0700

    added encryptTwoKeys method for more secure encryption

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -38,6 +38,43 @@ public class CaesarCipher {
         //Your answer is the String inside of encrypted
         return encrypted.toString();
     }
+    
+    public String encryptTwoKeys(String input, int key1, int key2) {
+        //Make a StringBuilder with message (encryptedTwoKeys)
+        StringBuilder encryptedTwoKeys = new StringBuilder(input);
+        //For each char in input, determine if odd or even idx
+        for (int k = 0; k < encryptedTwoKeys.length(); k++) {
+            //Get current character at current index k
+            char currChar = encryptedTwoKeys.charAt(k);
+            //Change the current character to a string for use in encrypt()
+            String currCharStr = Character.toString(currChar);
+            if (k % 2 == 0){
+                //If even index, encrypt() with key1, turn back to char, and add new character to answer
+                String newCharStr = encrypt(currCharStr, key1);
+                char newChar = newCharStr.charAt(0);
+                encryptedTwoKeys.setCharAt(k, newChar);
+            } else {
+                //If odd index, encrypt() with key2, turn back to char, and add new character to answer
+                String newCharStr = encrypt(currCharStr, key2);
+                char newChar = newCharStr.charAt(0);
+                encryptedTwoKeys.setCharAt(k, newChar);
+            }
+            
+        }
+        return encryptedTwoKeys.toString();
+    }
+    
+    public void testCaesarTwoKeys() {
+        int key1 = 1;
+        int key2 = 3;
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        String encryptedTwoKeys = encryptTwoKeys(message, key1, key2);
+        System.out.println(encryptedTwoKeys);
+        String decryptedTwoKeys = encryptTwoKeys(encryptedTwoKeys, 26-key1, 26-key2);
+        System.out.println(decryptedTwoKeys);
+    }
+    
     public void testCaesar() {
         int key = 17;
         FileResource fr = new FileResource();
