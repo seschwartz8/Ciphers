d062183daafa7c2ee32617ecc9e2643d32f0f4b5
Sasa Schwartz <seschwartz8@gmail.com>
2019-10-24 11:57:10 -0700

    added decrypt method for single key

--- CaesarCipher/CaesarCipher.java
+++ CaesarCipher/CaesarCipher.java
@@ -64,24 +64,73 @@ public class CaesarCipher {
         return encryptedTwoKeys.toString();
     }
     
-    public void testCaesarTwoKeys() {
-        int key1 = 1;
-        int key2 = 3;
-        FileResource fr = new FileResource();
-        String message = fr.asString();
-        String encryptedTwoKeys = encryptTwoKeys(message, key1, key2);
-        System.out.println(encryptedTwoKeys);
-        String decryptedTwoKeys = encryptTwoKeys(encryptedTwoKeys, 26-key1, 26-key2);
-        System.out.println(decryptedTwoKeys);
+    public int[] countLetters (String message) {
+        // SUMMARY: Counts occurrences of each letter of the alphabet in a string
+        int [] counts = new int[26];
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for(int k = 0; k < message.length(); k++) {
+            char ch = Character.toLowerCase(message.charAt(k));
+            int index = alphabet.indexOf(ch);
+            if (index != -1) { //If the character is a letter in the alphabet
+                counts[index] += 1;
+            }
+        }
+        return counts;
     }
     
-    public void testCaesar() {
+    public int maxIndex(int[] counts) {
+        // SUMMARY: Finds index with the highest count
+        // Iterate through counts to get highest count and note its index
+        int highest = 0;
+        int index = 0;
+        for (int i = 0; i < counts.length; i ++) {
+            if (counts[i] > highest) {
+                highest = counts[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    
+    public String decrypt (String encrypted) {
+        // SUMMARY: Decrypts string by finding the letter with highest frequency and assuming it's "E"
+        int[] letterCount = countLetters(encrypted);
+        int maxIdx = maxIndex(letterCount);
+        // Calculate the shift, assuming the letter with maxIdx is "E", which should be at idx 4 with no shift
+        int decryptKey = maxIdx - 4;
+        if (maxIdx < 4) {
+            decryptKey = 26 - (4 - maxIdx);
+        }
+        String decryptedMessage = encrypt(encrypted, 26-decryptKey);
+        return decryptedMessage;
+    }
+    
+    public void testCaesarEncrypt() {
+        // One key
         int key = 17;
         FileResource fr = new FileResource();
         String message = fr.asString();
         String encrypted = encrypt(message, key);
         System.out.println(encrypted);
         String decrypted = encrypt(encrypted, 26-key);
+        System.out.println(decrypted);
+        // Two keys
+        int key1 = 1;
+        int key2 = 3;
+        FileResource fr2 = new FileResource();
+        String message2 = fr2.asString();
+        String encryptedTwoKeys = encryptTwoKeys(message2, key1, key2);
+        System.out.println(encryptedTwoKeys);
+        String decryptedTwoKeys = encryptTwoKeys(encryptedTwoKeys, 26-key1, 26-key2);
+        System.out.println(decryptedTwoKeys);
+    }
+    
+    public void testCaesarDecrypt() {
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        String encryptedMessage = encrypt(message, 4);
+        System.out.println(encryptedMessage);
+        String decrypted = decrypt(message);
         System.out.println(decrypted);
     }
 }
